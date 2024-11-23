7638e1932ce9e4bcf53ecb5f243af91d7d66f214
Binh05 <147847203+Binh05@users.noreply.github.com>
2024-11-23 10:11:58 +0700

    Update MainForm.cs

--- StreetFighterGame/UI/MainForm.cs
+++ StreetFighterGame/UI/MainForm.cs
@@ -15,50 +15,9 @@ namespace StreetFighterGame
 {
     public partial class MainForm : Form
     {
-        private LogicGame logicGame;
-        private Timer animationTimer = new Timer();
         public MainForm()
         {
             InitializeComponent();
-            this.DoubleBuffered = true;
-            SetUpForm();
-        }
-        private void SetUpForm()
-        {
-            float gameWidth = this.ClientSize.Width;
-            float gameHeight = this.ClientSize.Height;
-
-            logicGame = new LogicGame(gameWidth, gameHeight);
-
-            gameTimer.Interval = 32;
-            gameTimer.Tick += GameLoop;
-            gameTimer.Start();
-
-            animationTimer.Interval = 100;
-            animationTimer.Tick += animatePlayer;
-            animationTimer.Start();
-
-            this.KeyDown += onKeyDown;
-            this.KeyUp += onKeyUp;
-        }
-        private void animatePlayer(object sender, EventArgs e)
-        {
-            logicGame.player.Update();
-        }
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            base.OnPaint(e);
-
-            Graphics Canvas = e.Graphics;
-
-            var player = logicGame.player;
-
-            Canvas.DrawImage(player.CurrentImage, new Rectangle((int)player.PositionX, (int)player.PositionY, player.width, player.height));
-        }
-        private void GameLoop(object sender, EventArgs e)
-        {
-            logicGame.Update();
-            Invalidate();
         }
         private void MainForm_Load(object sender, EventArgs e)
         {
@@ -66,20 +25,11 @@ namespace StreetFighterGame
         }
         private void onKeyDown(object sender, KeyEventArgs e)
         {
-            logicGame.onKeyDown(e.KeyCode);
+            
         }
         private void onKeyUp(object sender, KeyEventArgs e)
         {
-            switch(e.KeyCode)
-            {
-                case Keys.D:
-                    logicGame.player.stopMove();
-                    break;
-                case Keys.A:
-                    logicGame.player.stopMove();
-                    break;
-            }
-            logicGame.onKeyUp(e.KeyCode);
+            
         }
     }
 }
